746e134ec310304e9e6aa68280457e1329bbb184
ReginaldoDelarosa <rdelaros8@cuc.edu.co>
2023-11-29 01:29:05 -0500

    sisisi

--- src/components/salesEdit.jsx
+++ src/components/salesEdit.jsx
@@ -171,7 +171,7 @@ function SalesEdit(props) {
                 // set other product properties if needed
               }
             }}
-            value={usersId}
+            value={codigoProducto}
             className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             {products &&
@@ -240,21 +240,7 @@ function SalesEdit(props) {
             className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
-        <div className="mb-5">
-          <label
-            htmlFor="small-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Total venta
-          </label>
-          <input
-            type="number"
-            id="small-input"
-            onChange={(e) => setTotalVenta(e.target.value)}
-            value={totalVenta}
-            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-        </div>
+ 
         <div>
           <button
             onClick={id && handleUpdate}
